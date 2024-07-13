b87d2b654665a6140fb80b0f31e7fa22872f2567
HARSH21AT11 <harshsamarth21@gmail.com>
2024-07-13 15:38:14 +0530

    added react file with vite packege mannager

---
+++ typingtest-draft1/src/Seeker.jsx
@@ -0,0 +1,9 @@
+
+
+function seek_bar(){
+    return(
+        <h1>seeker is working!!</h1>
+    )
+}
+
+export default seek_bar
---
+++ typingtest-draft1/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Typing_App from './Typing_App'
+import Seeker from './Seeker'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+  
+  <Typing_App />
+  <Seeker/>
+  
+  </>
+)
